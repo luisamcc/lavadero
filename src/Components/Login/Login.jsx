1f5860f8df889d0b35926056f4c8114c94a010f5
luisamcc <luisamariana1998@yahoo.com.co>
2022-11-26 22:01:56 -0500

    login

--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,6 +1,47 @@
+import React from "react";
+import { InputText } from "primereact/inputtext";
+import { Button } from "primereact/button";
+import { Link } from "react-scroll";
+
 const Login = () => {
-    return (
-        <h1>Login</h1>
-    )
-}
+  return (
+    <>
+    <form>
+      <div>LoginForm</div>
+      <div className="login">
+        <center>
+        <div className="col-12 md:col-4" style={{ paddingLeft: "20px" }}>
+          <div className="p-inputgroup">
+            <span className="p-inputgroup-addon">
+              <i className="pi pi-user"></i>
+            </span>
+            <InputText placeholder="Username" />
+          </div>
+          <div className="p-inputgroup" style={{ paddingTop: "20px" }}>
+            <span className="p-inputgroup-addon">
+              <i className="pi pi-sign-in"></i>
+            </span>
+            <InputText
+              type="password"
+              name="password"
+              placeholder="Password"
+              value={""}
+            />
+          </div>
+        </div>
+        <div className="col-2 md:col-12" style={{ paddingTop: "20px" }}>
+            <Button type="submit" label="Submit" icon="pi pi-check" />
+        </div>
+        <div className="col-6">
+            <p>¿Aun no tienes una cuenta? <Link to='/registrarse'>Crear nueva cuenta</Link> </p>
+          </div>
+        </center>
+      </div>
+      </form>
+      
+    </>
+    //ToDo: inputs
+  );
+};
+
 export default Login;
