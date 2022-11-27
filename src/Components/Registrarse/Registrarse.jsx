da19f5b80738c63c66f7043c798ccb497e5cf2f8
luisamcc <luisamariana1998@yahoo.com.co>
2022-11-27 00:22:08 -0500

    registrarse ok

--- src/Components/Registrarse/Registrarse.jsx
+++ src/Components/Registrarse/Registrarse.jsx
@@ -27,7 +27,6 @@ const handleInputsForm = (e) => {
   // Inicio del manejo del handleDateRegister (Formulario de Registro)
   const handleOnSubmit = async ( e ) => {
     e.preventDefault();
-    alert(JSON.stringify(inputs));
   
     //envio de datos hacia el server 
     if (!firstname || !lastname || !email || !password) {
@@ -62,7 +61,7 @@ const handleInputsForm = (e) => {
         {/* Inicio del formulario de Registro */}
         <form action="" onSubmit={ handleOnSubmit}>
           <InputRegister title='first Name' type= 'text' name= 'firstname' value={inputs.firstname} handle= {handleInputsForm}/>
-          <InputRegister title='first Name' type= 'text' name= 'lastname' value={inputs.lastname} handle= {handleInputsForm}/>
+          <InputRegister title='last Name' type= 'text' name= 'lastname' value={inputs.lastname} handle= {handleInputsForm}/>
           <InputRegister title='Email' type='email' name='email' value={inputs.email} handle= {handleInputsForm}/>
           <InputRegister title='password' type='password' name='password' value={inputs.password} handle= {handleInputsForm}/>              
           <button type='submit' className="btn btn-primary" >
